2f5664811df095558df1c55cbd685ff511c98e9a
sametcanugur <sametcanugur@localhost>
2024-03-26 11:09:28 +0300

    Array of multiples creator program is done.

---
+++ MediumLevel/src/mediumExercises/ArrayofMultiples.java
@@ -0,0 +1,33 @@
+package mediumExercises;
+
+import java.util.Arrays;
+
+public class ArrayofMultiples {
+
+	public static void main(String[] args) {
+
+		System.out.println(Arrays.toString(arrayOfMultiplesCreator(12, 10)));
+	}
+
+	public static int[] arrayOfMultiplesCreator(int number, int length) {
+
+		String elementsOfArray = "";
+		for (int i = 1;; i++) {
+
+			elementsOfArray = elementsOfArray + (number * i) + ",";
+			if (number * i == number * length) {
+
+				break;
+			}
+		}
+
+		String[] stringArray = elementsOfArray.split(",");
+		int[] resultArray = new int[stringArray.length];
+		for (int j = 0; j < stringArray.length; j++) {
+
+			resultArray[j] = Integer.valueOf(stringArray[j]);
+		}
+
+		return resultArray;
+	}
+}
